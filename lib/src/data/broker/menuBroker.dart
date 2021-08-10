c024fc315e0466df8469a697161427ec2aede49e
virgerick <elfeo.0103@gmail.com>
2021-08-09 21:00:11 -0400

    menu broker

--- lib/src/data/broker/menuBroker.dart
+++ lib/src/data/broker/menuBroker.dart
@@ -3,15 +3,12 @@ import '../../../constants.dart';
 
 class MenuBroker {
   String baseUrl=apiUrl+"/api/menu";
-
+  MenuBroker();
   Future<http.Response> get(String filter) async {
      Uri url= Uri.parse(baseUrl);
     if(filter!=null)  
       url =Uri.parse(baseUrl+filter);
-   
     return await http.get(url);
   }
-
-
-  
+ 
 }
